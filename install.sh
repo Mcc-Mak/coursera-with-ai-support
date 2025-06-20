797453b24bb0b87c2ce7f6fe666b42fdd9daa4c1
Mcc-Mak <martinmcc5804@gmail.com>
2025-06-20 15:35:38 +0000

    commit

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 
 docker network create -d bridge --subnet=172.18.0.0/16 sso_auth_server 2>/dev/null
 
+# docker rm -f $(docker ps -a --format {{.Names}})
+# docker rmi -f $(docker images -aq)
+
 docker rmi $(docker images --filter dangling=true -aq) nil 2>/dev/null
 
 SRV="keycloak-mariadb-server"
@@ -19,7 +22,7 @@ SRV="reactjs-server"
 printf "\n\n[INFO] Installing '%s'..." "$SRV"
 cd $SRV
 bash INSTALL
-cd..
+cd ..
 
 SRV="flask-server"
 printf "\n\n[INFO] Installing '%s'..." "$SRV"
